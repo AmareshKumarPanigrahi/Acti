fc0ceb92403ece3c5a3c62ddf3fafd15bcb92814
AmareshKumarPanigrahi <amareshkumarpanigrahi@gmail.com>
2023-07-12 17:48:56 +0530

    pushing

---
+++ actitime/src/test/java/com/actitimeUser/User2Test.java
@@ -0,0 +1,38 @@
+package com.actitimeUser;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class User2Test {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ actitime/src/test/java/com/actitimeUser/User3Test.java
@@ -0,0 +1,36 @@
+package com.actitimeUser;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class User3Test {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		System.out.println("Amaresh");
+		
+		
+	}
+	
+
+}
