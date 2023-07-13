b7b71eee1f0f74be38a284757617e613e7887179
AmareshKumarPanigrahi <amareshkumaepanigrahi@gmail.com>
2023-07-13 16:18:32 +0530

    new new new commit

--- actitime/src/test/java/com/actitimeUser/User2Test.java
+++ actitime/src/test/java/com/actitimeUser/User2Test.java
@@ -2,17 +2,25 @@ package com.actitimeUser;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class User2Test {
 
 	public static void main(String[] args) {
+		String browser="";
 		WebDriverManager.chromedriver().setup();
+		WebDriverManager.edgedriver().setup();
 		
+		if(browser.equals("chrome")) {
 		WebDriver driver =new ChromeDriver();
 		driver.get("facebook.com");
-	    
+		}
+		else if(browser.equals("edge")) {
+			WebDriver driver =new EdgeDriver();
+			driver.get("facebook.com");
+		}
 		
 				
 		
--- actitime/src/test/java/com/actitimeUser/UsernameTest.java
+++ actitime/src/test/java/com/actitimeUser/UsernameTest.java
@@ -1,13 +1,26 @@
 package com.actitimeUser;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UsernameTest {
 	
 	public static void main(String[] args) {
+		String browser="";
 		WebDriverManager.chromedriver().setup();
+		WebDriverManager.edgedriver().setup();
 		
-		System.out.println("Amaresh");
+		if(browser.equals("chrome")) {
+		WebDriver driver =new ChromeDriver();
+		driver.get("facebook.com");
+		}
+		else if(browser.equals("edge")) {
+			WebDriver driver =new EdgeDriver();
+			driver.get("facebook.com");
+		}
 		
 		
 		
